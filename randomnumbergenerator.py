a1176d02e7bec9369ab2f804d393e09b7514ed52
Louie Schiltz <Lfschiltz95@gmail.com>
2025-07-27 14:39:25 -0500

    added randomnumbergenerator.py

---
+++ randomnumbergenerator.py
@@ -0,0 +1,8 @@
+import random
+# the use of the randint() function
+number1 = random.randint(1, 100)        # an int from 1 to 100
+print(number1)
+number2 = random.randint(101, 200) 	# an int from 101 to 200
+print(number2)
+number3 = random.randint(0, 7) 		# an int from 0 to 7
+print(number3)
